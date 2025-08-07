27be0a7b7ac6155478a4fb0d5cb3b6f5ee54adac
aashim <thelastmanarmy37@gmail.com>
2025-08-07 10:35:16 +0000

    checkpoint

--- backend/src/types/WebHookHandlers.ts
+++
@@ -1,9 +0,0 @@
-import { Octokit } from "octokit";
-import {
-  EmitterWebhookEventName,
-  EmitterWebhookEvent,
-} from "@octokit/webhooks";
-
-export type WebhookHandlerArgs<E extends EmitterWebhookEventName> = {
-  octokit: Octokit;
-} & EmitterWebhookEvent<E>;
